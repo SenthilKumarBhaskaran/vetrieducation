3148249e147a1b40f9a5e2aef071aa61997ed19f
Senthil Kumar <senthil@linuxmint.linuxmint-domain>
2014-04-16 11:45:38 -0400

    Adding Vetri site to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vetrieducation::Application.config.secret_token = 'bde8dad4b4b0845473211a287368b5de1c432cca223862451ad0eacfbb362c64f0db65dc6c17eada001e71a1298d0bf1057eeb432e1cdfe0a219a5967f1962f6'
